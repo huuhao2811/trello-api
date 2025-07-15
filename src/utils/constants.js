7de266652be11971863aacd555fa9b2264bf6cd8
Nguyễn Hữu Hào <huuhao28112005@gmail.com>
2025-07-15 19:58:15 +0700

    change whitelist again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const WHITELIST_DOMAINS = [
     //'http://localhost:5173'
-    'https://trello-web-full.vercel.app/'
+    'https://trello-web-full.vercel.app'
 ]
 
 export const BOARD_TYPES = {
